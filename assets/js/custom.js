3dc4b7028ee631c8ee5502d7ae0983a6417bfaa6
mohammed derfoufi <simo.derfoufi.99@gmail.com>
2023-10-03 00:19:20 +0200

    servicio terminado

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,19 @@
+/* National Services */ 
+let national_form = document.getElementById('national_form');
+let nationalButton = document.getElementById('national_button');
+
+function hideNationalForm() {
+    national_form.classList.remove('d-none');
+    nationalButton.classList.add('d-none');
+}
+
+function cancelNationalForm() {
+    national_form.classList.add('d-none');
+    nationalButton.classList.remove('d-none');
+}
+
+
+/* Airport Services */
 let booking_form = document.getElementById('booking_form');
 let display = 0;
 let buttonForm = document.getElementById('button_form');
@@ -13,6 +29,19 @@ function cancelForm() {
     buttonForm.classList.remove('d-none');
 }
 
+/* Renfe Services */ 
+let renfe_form = document.getElementById('renfe_form');
+let renfeButton = document.getElementById('renfe_button');
+
+function hideRenfeForm() {
+    renfe_form.classList.remove('d-none');
+    renfeButton.classList.add('d-none');
+}
+
+function cancelRenfeForm() {
+    renfe_form.classList.add('d-none');
+    renfeButton.classList.remove('d-none');
+}
 
 let originSelect = document.getElementById('origin-select');
 let dropSelect = document.getElementById('drop-select');
@@ -39,6 +68,31 @@ originSelect.addEventListener('change', function() {
     }
 });
 
+let originStation = document.getElementById('origin-station');
+let dropStation = document.getElementById('drop-station');
+
+originStation.addEventListener('change', function() {
+    let selectedValue = originStation.value;
+    dropStation.innerHTML = '';
+    let option = document.createElement('option');
+    option.text = 'Select drop location';
+    dropStation.add(option);
+
+    for (let i = 1; i < originStation.options.length; i++) {
+        let option = document.createElement('option');
+        option.value = originStation.options[i].value;
+        option.text = originStation.options[i].text;
+        dropStation.add(option);
+    }
+
+    for (let i = 0; i < dropStation.options.length; i++) {
+        if (dropStation.options[i].value === selectedValue) {
+            dropStation.remove(i);
+          break;
+        }
+    }
+});
+
 let adult = document.getElementById('adult_selection');
 let children = document.getElementById('child_selection');
 const taxi_passengers = 7
@@ -56,3 +110,4 @@ adult.addEventListener('change', function() {
         children.add(option);
     }
 });
+
